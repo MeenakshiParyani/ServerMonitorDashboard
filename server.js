ceed6f5e5b31c5674f8d7ddd27760636963417b3
MeenakshiParyani <meenakshi.paryani@sjsu.edu>
2017-04-10 11:23:41 -0700

    Getting memory usage statistics

--- server.js
+++ server.js
@@ -11,15 +11,22 @@ var methodOverride = require('method-override');
 var db = require('./config/db');
 
 var port = process.env.PORT || 8080; // set our port
-// mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
+
 
 // get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json 
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+
+var api = require('./public/js/routes');
+app.use('/usage', api);
+
 
 // routes ==================================================
 require('./app/routes')(app); // pass our application into our routes
